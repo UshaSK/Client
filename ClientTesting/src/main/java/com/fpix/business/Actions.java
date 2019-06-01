a2a8577c5dec10174492e4d6d1acf6510082105c
PrashanthiAdmin <prashanthi.skvl@gmail.com>
2019-06-01 20:02:01 +0530

    First Commit

---
+++ ClientTesting/src/main/java/com/fpix/business/Actions.java
@@ -0,0 +1,15 @@
+package com.fpix.business;
+
+public class Actions {
+@Override
+public String toString() {
+	// TODO Auto-generated method stub
+	return super.toString();
+}
+
+public static void main(String[] args) {
+	
+	System.out.println("dddjj");
+}
+
+}
---
+++ ClientTesting/src/test/java/Client/ClientTesting/Test1.java
@@ -0,0 +1,8 @@
+package Client.ClientTesting;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
+}
